1bc099dfd9d5c05b18998d80451079263607d622
pratik <yadavpratik8806@gmail.com>
2022-12-24 00:34:13 +0530

    added sweetalert as a helper function

--- src/Helper Functions/sweetAlerts.js
+++ src/Helper Functions/sweetAlerts.js
@@ -1,5 +1,13 @@
 import swal from 'sweetalert'
 
+export const normalAlert = (title, icon, text) => {
+    swal({
+        title : title,
+        text : text,
+        icon : icon
+    })
+}
+
 export const handleLogout = (logoutAndRedirect) => {
     swal({
         title : 'Sure you wanna Logout?',
